b1f0e2b0849bb769b824bb2a723ed5126b1f43e9
dell <sxy759334746@126.com>
2024-06-27 15:59:23 +0800

    commit 接口文档调试 jwt令牌 threadlocal获取登录用户id

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ java外卖业务项目学习
 使用SpringBoot Redis MyBatis MySQL Nginx SpringTask Swagger JWT 
 
 前端微信小程序
+
+
+## Nginx
+
+- 反向代理配置
+- 负载均衡：
+权重，ip哈希，最少连接，url哈希，响应时间
+
+
+## 登录密码加密
+
+- 返回password MD5加密
+
+
+## apifox
+
+- 导入接口文档
+
+
+## Swagger
+
+- knife4j 生成接口文档
+- 常用注解：@Api @ApiModel @ApiModelProperty @ApiOperation
+- 在doc.html 进行调试，添加全局参数token
+
+## 员工数据CURD
+
+- 全局异常处理
+- jwt获取当前登录用户id
+- ThreadLocal set() get() remove()
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,22 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+
+    /**
+     * 处理员工username重复异常
+     * TODO 这里判断方法或许可以改成查询数据库返回重复插入值
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String msg = MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
